8ad5fa2ff428b8491cc4cac20241e537ff9a1ad6
Axelrodde <umartiamiyu@gmail.com>
2022-09-08 21:35:01 -0700

    what do we have here

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
+ * main - print the string in the put function
  *
- * main - main block
  * Description: using the main function
- * this program prints 
+ * this program prints 'Programming is like building a multilingual puzzle' 
  * Return: 0
  */
 int main(void)
