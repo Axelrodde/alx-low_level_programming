3e6aea62f9ce5f1ced8432778a3f2cda5aef0159
Axelrodde <umartiamiyu@gmail.com>
2022-09-12 19:59:53 -0700

    lfg

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,16 +8,12 @@
  *
  *  * main - Entry point
  *
- *   *
+ * 
  *
- *    * Return: return 1
+ *  Return: return 1
  *
- *     */
-
-
-
+ */
 int main(void)
-
 {
 
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
